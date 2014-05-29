f40c91d37f2a5b68733eb92a43a87e20b0f65e83
Julio Makdisse Saito <saitodisse@gmail.com>
2014-05-29 08:55:03 -0300

    a simple test unit test

--- README.md
+++ README.md
@@ -2,3 +2,11 @@ meld-trace-logger
 =================
 
 meld + trace + colorfull console.log
+
+
+##Tests
+```
+sudo npm i nodeunit -g
+sudo npm i jshint -g
+npm test
+```
--- meld-trace-logger.js
+++ meld-trace-logger.js
@@ -17,130 +17,138 @@ var __LOG = function __LOG(message, thisObj, args, colorIndex){
   var groupName
     , groupColor
 
-      , rpad = function (str, padString, length) {
-              while (str.length < length) {
-                  str = str + padString;
-              }
-              return str;
-          }
-
-      , truncate = function (str, length, truncateStr) {
-              if (str === null) {
-                  return '';
-              }
-              str = String(str);
-              truncateStr = truncateStr || '...';
-              length = ~~length;
-              return str.length > length ? str.slice(0, length) + truncateStr : str;
-          }
-
-      , getName = function (options) {
-              var nameParts
-                  , name = ''
-                  , method = ''
-              ;
-
-              if (options.name) {
-                  nameParts = options.name.split('.');
-              }
-              else {
-                  return '';
-              }
-              
-              // get the first part
-              name = nameParts.shift();
-              name = truncate(name, (options.nameSize - 2), '..');
-
-              if (nameParts.length > 0) {
-                  var fullMethodName = nameParts.join('.');
-                  fullMethodName = getDetailsByMethodName(fullMethodName);
-                  method = truncate(fullMethodName, (options.methodSize - 2), '..');
-
-                  var hasId = _.isObject(thisObj) && _.has(thisObj, 'id');
-                  if (hasId) {
-                      name = name + '[' + thisObj.id + ']';
-                  }
-
-                  name   = rpad(name,   ' ', options.nameSize);
-                  method = rpad(method, ' ', options.methodSize);
-              }
-              else {
-                  throw new Error('getName :: cant find "." to split method');
-              }
-
-              return name + method;
-          }
-
-      , getDetailsByMethodName = function (name) {
-          if(args && typeof args === 'object' &&args.length > 0){
-            return rpad(name,   ' ', 20) + getArgs(args);
-          }
-          return name;
-        }
-
-      , getArgs = function(args) {
-          var result = '';
-          for (var i = 0; i < args.length; i++) {
-            if(typeof args[i] === 'string'){
-              result += ' ["' + args[i] + '"]'
+    , rpad = function (str, padString, length) {
+            while (str.length < length) {
+                str = str + padString;
             }
-          }
-          return result;
+            return str;
         }
 
-      , colors = {
-          10: {  backgroundColor: '#eee'
-               , foregroundColor: '#600'
-          },
+    , truncate = function (str, length, truncateStr) {
+            if (str === null) {
+                return '';
+            }
+            str = String(str);
+            truncateStr = truncateStr || '...';
+            length = ~~length;
+            return str.length > length ? str.slice(0, length) + truncateStr : str;
+        }
 
-          11: {  backgroundColor: '#eee'
-               , foregroundColor: '#330'
-          },
+    , _isObject = function(obj) {
+            return obj === Object(obj);
+    }
 
-          12: {  backgroundColor: '#eee'
-               , foregroundColor: '#033'
-          },
+    , _has = function(obj, key) {
+            return hasOwnProperty.call(obj, key);
+    }
 
-          20: {  backgroundColor: '#ded'
-              , foregroundColor: '#260'
-          },
+    , getName = function (options) {
+            var nameParts
+                , name = ''
+                , method = ''
+            ;
 
-          21: {  backgroundColor: '#ded'
-              , foregroundColor: '#243'
-          },
+            if (options.name) {
+                nameParts = options.name.split('.');
+            }
+            else {
+                return '';
+            }
+            
+            // get the first part
+            name = nameParts.shift();
+            name = truncate(name, (options.nameSize - 2), '..');
 
-          22: {  backgroundColor: '#ded'
-              , foregroundColor: '#225'
-          },
+            if (nameParts.length > 0) {
+                var fullMethodName = nameParts.join('.');
+                fullMethodName = getDetailsByMethodName(fullMethodName);
+                method = truncate(fullMethodName, (options.methodSize - 2), '..');
 
-          3: {  backgroundColor: '#eee'
-              , foregroundColor: '#21460F'
-          },
+                var hasId = _isObject(thisObj) && _has(thisObj, 'id');
+                if (hasId) {
+                    name = name + '[' + thisObj.id + ']';
+                }
 
-          4: {  backgroundColor: '#eee'
-              , foregroundColor: '#6700B9'
-          },
-          
-          5: {  backgroundColor: '#eee'
-              , foregroundColor: '#284'
-          },
-          
-          6: {  backgroundColor: '#eee'
-              , foregroundColor: '#7653C1'
-          },
-          
-          99: {  backgroundColor: '#222'
-              , foregroundColor: '#FBB'
-          }
+                name   = rpad(name,   ' ', options.nameSize);
+                method = rpad(method, ' ', options.methodSize);
+            }
+            else {
+                throw new Error('getName :: cant find "." to split method');
+            }
+
+            return name + method;
+        }
+
+    , getDetailsByMethodName = function (name) {
+        if(args && typeof args === 'object' &&args.length > 0){
+          return rpad(name,   ' ', 20) + getArgs(args);
+        }
+        return name;
       }
 
-      , logGroup = function ()
-          {
-              console.groupCollapsed(groupName, groupColor);
-              console.dir(thisObj);
-              console.dir(args);
-              console.groupEnd();
+    , getArgs = function(args) {
+        var result = '';
+        for (var i = 0; i < args.length; i++) {
+          if(typeof args[i] === 'string'){
+            result += ' ["' + args[i] + '"]';
           }
+        }
+        return result;
+      }
+
+    , colors = {
+        10: {  backgroundColor: '#eee'
+             , foregroundColor: '#600'
+        },
+
+        11: {  backgroundColor: '#eee'
+             , foregroundColor: '#330'
+        },
+
+        12: {  backgroundColor: '#eee'
+             , foregroundColor: '#033'
+        },
+
+        20: {  backgroundColor: '#ded'
+            , foregroundColor: '#260'
+        },
+
+        21: {  backgroundColor: '#ded'
+            , foregroundColor: '#243'
+        },
+
+        22: {  backgroundColor: '#ded'
+            , foregroundColor: '#225'
+        },
+
+        3: {  backgroundColor: '#eee'
+            , foregroundColor: '#21460F'
+        },
+
+        4: {  backgroundColor: '#eee'
+            , foregroundColor: '#6700B9'
+        },
+        
+        5: {  backgroundColor: '#eee'
+            , foregroundColor: '#284'
+        },
+        
+        6: {  backgroundColor: '#eee'
+            , foregroundColor: '#7653C1'
+        },
+        
+        99: {  backgroundColor: '#222'
+            , foregroundColor: '#FBB'
+        }
+    }
+
+    , logGroup = function ()
+        {
+            console.groupCollapsed(groupName, groupColor);
+            console.dir(thisObj);
+            console.dir(args);
+            console.groupEnd();
+        }
   ;
 
   var ignores = message.search(/\bgetWildcardCallbacks\b/) >= 0;
@@ -186,8 +194,9 @@ var __MELD_LOG = function(appName, app, colorIndex){
     counter: 0,
     lpad: function (str, padString, length) {
       str = String(str);
-      while(str.length < length)
-          str = padString + str;
+      while(str.length < length){
+        str = padString + str;
+      }
       return str;
     },
     getCounter: function ()
@@ -211,3 +220,5 @@ var __MELD_LOG = function(appName, app, colorIndex){
   // Around advice wraps the intercepted method in layers
   meld(app, pointcut, trace(myReporter));
 };
+
+module.export = __MELD_LOG;
