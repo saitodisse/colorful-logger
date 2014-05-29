437548882b765dbb7e9d7d4a450a7348cfeebd8b
Julio Makdisse Saito <saitodisse@gmail.com>
2014-05-29 09:36:28 -0300

    some tests and some README explanations

--- README.md
+++ README.md
@@ -4,9 +4,26 @@ meld-trace-logger
 meld + trace + colorfull console.log
 
 
-##Tests
+##pre-requirements
 ```
-sudo npm i nodeunit -g
-sudo npm i jshint -g
-npm test
+sudo npm i jshint nodeunit supervisor -g
 ```
+
+##nodeunit
+```
+nodeunit tests/
+```
+
+##nodeunit + node-debug
+from: http://stackoverflow.com/questions/16652358/how-to-debug-nodeunit-using-node-inspector
+```
+npm -g install supervisor node-inspector
+
+# console 1: supervisor restarts node-inspector when it quits, ignores file changes
+supervisor -i . -x node-inspector .
+
+# console 2
+supervisor --debug-brk -- `which nodeunit` tests/
+```
+
+
--- tests/logger.test.js
+++ tests/logger.test.js
@@ -1,10 +1,25 @@
-exports.testSomething = function(test){
-    test.expect(1);
-    test.ok(true, 'this assertion should pass');
-    test.done();
-};
+var colorfullLogger = require('../colorfull-logger');
 
-exports.testSomethingElse = function(test){
-    test.ok(false, 'this assertion should fail');
-    test.done();
+module.exports = {
+    setUp: function (callback) {
+	    console.groupCollapsed = function(){};
+	    console.groupEnd = function(){};
+	    console.debug = function(){};
+        callback();
+    },
+    tearDown: function (callback) {
+        // clean up
+        callback();
+    },
+
+    isFunction: function (test) {
+	    test.equal('function', typeof colorfullLogger);
+	    test.done();
+    },
+
+    canBeCalled: function (test) {
+	    colorfullLogger('Namespace.method()', this, arguments, 10);
+	    test.ok(true, 'errors occurred');
+	    test.done();
+    }
 };
