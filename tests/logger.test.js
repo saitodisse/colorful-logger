3b66b6fee80591a7b5bcc1241253cb495f0dcacf
Julio Makdisse Saito <saitodisse@gmail.com>
2014-05-30 12:58:43 -0300

    restarting from scratch using TDD

--- tests/logger.test.js
+++ tests/logger.test.js
@@ -1,55 +1,74 @@
 var colorfullLogger = require('../src/colorfull-logger');
 
-function enableFakeConsole(f, test) {
-    var oldLog = console;
+function enableFakeConsole(colorfullLoggerCall, testCallback) {
+	//bkp original console
+	var oldLog = console;
+	console.debug = function(str){
+		testCallback(str);
+	};
 
-    console.groupCollapsed = function(groupName, groupColor){
-        console.log('groupName:[' + groupName + '], groupColor:[' + groupColor + ']');
-    };
-    console.groupEnd = function(){
-        console.log('groupEnd:');
-    };
-    console.debug = function(str){
-        console.log('debug:', str);
-    };
-    console.dir = function(obj){
-        console.log('dir:', obj);
-    };
+	//call the logger
+	colorfullLoggerCall();
 
-    // console.log = function(str) {
-    //     console.log('log:', str);
-    // };
+	//restore original console
+	console = oldLog;
 
-    test.ok(true, 'test.ok(true)');
+	// var oldLog = console;
 
-    f();
+	// console.groupCollapsed = function(groupName, groupColor){
+	// 	console.log('groupName:[' + groupName + '], groupColor:[' + groupColor + ']');
+	// };
+	// console.groupEnd = function(){
+	// 	console.log('groupEnd:');
+	// };
+	// console.dir = function(obj){
+	// 	console.log('dir:', obj);
+	// };
 
-    console = oldLog;
+	// test.ok(true, 'test.ok(true)');
+
+	// f();
+
 }
 
 module.exports = {
-    setUp: function (callback) {
-        callback();
-    },
-    tearDown: function (callback) {
-        callback();
-    },
+	setUp: function (callback) {
+		callback();
+	},
+	tearDown: function (callback) {
+		callback();
+	},
 
-    colorfullLogger_is_a_function: function (test) {
-	    test.equal('function', typeof colorfullLogger,
-            'must be a function');
-	    test.done();
-    },
+	colorfullLogger_is_a_function: function (test) {
+		test.equal('function', typeof colorfullLogger,
+			'must be a function');
+		test.done();
+	},
 
-    can_log_a_simple_object: function (test) {
-        enableFakeConsole(function() {
-            var aSimpleObj = {
-                property1: 'property 1'
-            };
-            var argumentsSample = {'0':1,'1':'2','2':[3]};
-            colorfullLogger('Namespace.ObjectMethodName()', aSimpleObj, argumentsSample, 10);
-        }, test);
-        
-	    test.done();
-    }
+	call_console_debug: function(test) {
+		var message = 'this must be printed';
+		enableFakeConsole(
+			function() {
+				colorfullLogger(message);
+			},
+			function(consoleResult) {
+				test.equal(message, consoleResult,
+					'console debug must be called with: "this must be printed"');
+				test.done();
+			}
+		);
+
+	},
+
+	// can_log_a_simple_object: function (test) {
+	//     enableFakeConsole(function() {
+	//         var aSimpleObj = {
+	//             property1: 'property 1'
+	//         };
+	//         var argumentsSample = {'0':1,'1':'2','2':[3]};
+	//         colorfullLogger('Namespace.ObjectMethodName()', aSimpleObj, argumentsSample, 10);
+	//     }, test);
+		
+	   //  test.done();
+	// }
 };
